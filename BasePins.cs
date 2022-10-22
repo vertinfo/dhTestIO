9921f6de5bf98e0fd9b8f8eb495d99b9cd202cda
Dan Bryant <dbryant@ii4tech.com>
2022-10-22 16:22:26 -0500

    cleaned

---
+++ BasePins.cs
@@ -0,0 +1,19 @@
+﻿
+namespace dhTestIO
+{
+    public enum S6Base
+    {
+        DSL = 0,
+        USL = 1,
+        SP3 = 2,
+        DCL = 3
+    }
+
+    public enum S5Base
+    {
+        DOL = 0,
+        DCL = 1,
+        INT = 2,
+        PWR = 3
+    }
+}
--- Relays.cs
+++ Relays.cs
@@ -1,10 +1,10 @@
 ﻿using System.Device.Gpio;
 using System.Device.I2c;
-
+using System.Security.AccessControl;
 
 namespace dhTestIO
 {
-    internal class Relays
+    public class Relays
     {
         I2cDevice i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1, 0x20));
         GpioController controller = new GpioController(PinNumberingScheme.Board);
@@ -12,11 +12,11 @@ namespace dhTestIO
         public Relays()
         {
             Console.WriteLine($"i2c = {i2cDevice.ConnectionSettings.DeviceAddress}");
-            controller.OpenPin(0xff &= 0x1 << (int)Pins.USL, PinMode.Output);
+            controller.OpenPin(0x1 << (int)S6Base.USL, PinMode.Output);
             Console.WriteLine($"gpio numbering = {controller.NumberingScheme}");
-            controller.OpenPin(0xff &= 0x1 << (int)Pins.DSL, PinMode.Output);
-            controller.OpenPin(0xff &= 0x1 << (int)Pins.SP3, PinMode.Output);
-            controller.OpenPin(0xff &= 0x1 << (int)Pins.DCL, PinMode.Output);
+            controller.OpenPin(0x2 << (int)S6Base.DSL, PinMode.Output);
+            controller.OpenPin(0x3 << (int)S6Base.SP3, PinMode.Output);
+            controller.OpenPin(0x4 << (int)S6Base.DCL, PinMode.Output);
             Console.WriteLine($"gpio pin count = {controller.PinCount}");
             Console.WriteLine($"gpio numbering = {controller.NumberingScheme}");
         }
@@ -25,50 +25,50 @@ namespace dhTestIO
         {
             Console.WriteLine("ON 1...");
             
-            controller.Write(0xff &= 0x1 << (int)Pins.USL, PinValue.High);
+            controller.Write(0x1 << (int)S6Base.USL, PinValue.High);
         }
 
         public void ON_2()
         {
             Console.WriteLine("ON 2...");
-            controller.Write(0x1 << (int)Pins.DSL, PinValue.High);
+            controller.Write(0x2 << (int)S6Base.DSL, PinValue.High);
         }
 
         public void ON_3()
         {
             Console.WriteLine("ON 3...");
-            controller.Write(0x1 << (int)Pins.SP3, PinValue.High);
+            controller.Write(0x3 << (int)S6Base.SP3, PinValue.High);
         }
 
         public void ON_4()
         {
             Console.WriteLine("ON 4...");
-            controller.Write(0x1 << (int)Pins.DCL, PinValue.High);
+            controller.Write(0x4 << (int)S6Base.DCL, PinValue.High);
         }
 
         public void OFF_1()
         {
             Console.WriteLine("OFF 1...");
-            controller.Write(0x1 << (int)Pins.USL, PinValue.Low);
+            controller.Write(0x1 << (int)S6Base.USL, PinValue.Low);
         }
 
         public void OFF_2()
         {
             Console.WriteLine("ON 2...");
-            controller.Write(0x1 << (int)Pins.DSL, PinValue.Low);
+            controller.Write(0x2 << (int)S6Base.DSL, PinValue.Low);
         }
 
         public void OFF_3()
         {
             Console.WriteLine("OFF 3...");
-            controller.Write(0x1 << (int)Pins.SP3, PinValue.Low);
+            controller.Write(0x3 << (int)S6Base.SP3, PinValue.Low);
 
         }
 
         public void OFF_4()
         {
             Console.WriteLine("OFF 4...");
-            controller.Write(0x1 << (int)Pins.DCL, PinValue.Low);
+            controller.Write(0x4 << (int)S6Base.DCL, PinValue.Low);
 
         }
     }
