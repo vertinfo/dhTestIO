2febe19c6caf843c463b0263dfd067f23bfc28e6
Dan Bryant <dbryant@ii4tech.com>
2022-10-28 10:07:51 -0500

    added i2c object and writes

--- Relays.cs
+++ Relays.cs
@@ -8,67 +8,84 @@ namespace dhTestIO
     {
         I2cDevice i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1, 0x20));
         GpioController controller = new GpioController(PinNumberingScheme.Board);
-        
+        Byte payload = 0xff;
         public Relays()
         {
             Console.WriteLine($"i2c = {i2cDevice.ConnectionSettings.DeviceAddress}");
-            controller.OpenPin(0x1 << (int)S6Base.USL, PinMode.Output);
+            
+            //controller.OpenPin(0x1 << (int)S6Base.USL, PinMode.Output);
             Console.WriteLine($"gpio numbering = {controller.NumberingScheme}");
-            controller.OpenPin(0x2 << (int)S6Base.DSL, PinMode.Output);
-            controller.OpenPin(0x3 << (int)S6Base.SP3, PinMode.Output);
-            controller.OpenPin(0x4 << (int)S6Base.DCL, PinMode.Output);
+            //controller.OpenPin(0x2 << (int)S6Base.DSL, PinMode.Output);
+            //controller.OpenPin(0x3 << (int)S6Base.SP3, PinMode.Output);
+            //controller.OpenPin(0x4 << (int)S6Base.DCL, PinMode.Output);
             Console.WriteLine($"gpio pin count = {controller.PinCount}");
             Console.WriteLine($"gpio numbering = {controller.NumberingScheme}");
         }
 
         public void ON_1()
         {
+            payload = 0x1 << 0;
             Console.WriteLine("ON 1...");
-            
-            controller.Write(0x1 << (int)S6Base.USL, PinValue.High);
+            /* Writing a byte to the I2C device. */
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x1 << (int)S6Base.USL, PinValue.High);
         }
 
         public void ON_2()
         {
+            payload = 0x1 << 1;
             Console.WriteLine("ON 2...");
-            controller.Write(0x2 << (int)S6Base.DSL, PinValue.High);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x2 << (int)S6Base.DSL, PinValue.High);
         }
 
         public void ON_3()
         {
+            payload = 0x1 << 2;
             Console.WriteLine("ON 3...");
-            controller.Write(0x3 << (int)S6Base.SP3, PinValue.High);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x3 << (int)S6Base.SP3, PinValue.High);
         }
 
         public void ON_4()
         {
+            payload = 0x1 << 3;
             Console.WriteLine("ON 4...");
-            controller.Write(0x4 << (int)S6Base.DCL, PinValue.High);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x4 << (int)S6Base.DCL, PinValue.High);
         }
 
         public void OFF_1()
         {
+            payload = 0x1 << 0;
             Console.WriteLine("OFF 1...");
-            controller.Write(0x1 << (int)S6Base.USL, PinValue.Low);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x1 << (int)S6Base.USL, PinValue.Low);
         }
 
         public void OFF_2()
         {
+            payload = 0x1 << 1;
             Console.WriteLine("ON 2...");
-            controller.Write(0x2 << (int)S6Base.DSL, PinValue.Low);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x2 << (int)S6Base.DSL, PinValue.Low);
         }
 
         public void OFF_3()
         {
+            payload = 0x1 << 2;
             Console.WriteLine("OFF 3...");
-            controller.Write(0x3 << (int)S6Base.SP3, PinValue.Low);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x3 << (int)S6Base.SP3, PinValue.Low);
 
         }
 
         public void OFF_4()
         {
+            payload = 0x1 << 3;
             Console.WriteLine("OFF 4...");
-            controller.Write(0x4 << (int)S6Base.DCL, PinValue.Low);
+            i2cDevice.WriteByte(payload);
+            //controller.Write(0x4 << (int)S6Base.DCL, PinValue.Low);
 
         }
     }
